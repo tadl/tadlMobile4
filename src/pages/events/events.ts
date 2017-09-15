741480c4a59cc9db2b27f4bd76430c9e173e9489
William Rockwood <wrockwood@tadl.org>
2017-09-15 12:39:32 -0400

    events.ts readability
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- src/pages/events/events.ts
+++ src/pages/events/events.ts
@@ -15,11 +15,14 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class EventsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    constructor(
+        public navCtrl: NavController,
+        public navParams: NavParams
+    ) {
+    }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad EventsPage');
-  }
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad EventsPage');
+    }
 
 }
