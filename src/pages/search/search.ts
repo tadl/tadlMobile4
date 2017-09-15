bf2e43375f0ff1cea8022355b0798940ff36eef1
Scott Morey <swmorey@gmail.com>
2017-09-15 16:02:48 -0400

    work on search, you can do them from main page

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController} from 'ionic-angular';
+import { Http } from '@angular/http';
+import { Item } from '../../app/item'
+import 'rxjs/add/operator/map';
+
 
 @IonicPage()
 @Component({
@@ -8,14 +12,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SearchPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController, 
+  	public navParams: NavParams,
+  	private http: Http,
+  	public item: Item,
+  	public loadingCtrl: LoadingController,
+  ) {}
 
   advanced: boolean = this.navParams.get('advanced') || false
   query: string = this.navParams.get('query')
+  results:Array<{any}> = []
+  page: number = 0
+
+
+	get_results(){
+  		let loading = this.loadingCtrl.create({content:'Searching...'})
+  		loading.present()
+  		this.http.get('https://catalog.tadl.org/search.json?query=' + this.query).map(res => res.json()).subscribe(data=>{
+			loading.dismiss()
+			if(data.items){
+				this.results = data.items
+			}else{
+			}
+  		});
+  	}
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SearchPage');
+    if(this.navParams.get('query')){
+    	this.get_results()
+    }
   }
 
 }
