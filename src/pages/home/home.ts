680cc41c6b219cdc9425c9063181f651bdfb9af9
Scott Morey <swmorey@gmail.com>
2017-09-18 13:36:49 -0400

    events page working!

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,7 @@ import { NavController, Nav } from 'ionic-angular';
 import { Globals } from '../../app/globals';
 import { CheckoutsPage } from '../checkouts/checkouts';
 import { SearchPage } from '../search/search';
+import { EventsPage } from '../events/events';
 
 @Component({
   selector: 'page-home',
@@ -12,6 +13,7 @@ import { SearchPage } from '../search/search';
 export class HomePage {
     searchPage = SearchPage
     checkoutsPage = CheckoutsPage
+    eventsPage = EventsPage
 
     query: string
 
