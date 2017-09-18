1992af8b44222643d727e08ad0dcc16044af0891
William Rockwood <wrockwood@tadl.org>
2017-09-18 15:26:05 -0400

    infinite scrolling events!
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- src/pages/events/events.ts
+++ src/pages/events/events.ts
@@ -20,17 +20,46 @@ export class EventsPage {
     ) { }
 
     events: Array<{any}> = [];
+    url: string = "https://www.tadl.org/wp-json/tribe/events/v1/events?start_date=now";
+    next_url: string;
+    prev_url: string;
 
     get_events() {
         let loading = this.loadingCtrl.create({content:'Loading events...'});
         loading.present();
-        this.http.get('https://www.tadl.org/wp-json/tribe/events/v1/events?start_date=now').map(res => res.json()).subscribe(data=>{
+        this.http.get(this.url).map(res => res.json()).subscribe(data=>{
             loading.dismiss();
             if (data.events) {
                 this.events = data.events;
-            } else {
+            }
+            if (data.next_rest_url) {
+                this.next_url = data.next_rest_url;
+            }
+            if (data.previous_rest_url) {
+                this.prev_url = data.previous_rest_url;
             }
         });
+    }
+
+    doInfinite(infiniteScroll) {
+        console.log('begin async operation');
+        setTimeout(() => {
+            this.url = this.next_url;
+            this.http.get(this.url).map(res => res.json()).subscribe(data=>{
+                if (data.events) {
+                    this.events.push.apply(this.events, data.events);
+                }
+                if (data.next_rest_url) {
+                    this.next_url = data.next_rest_url;
+                }
+                if (data.previous_rest_url) {
+                    this.prev_url = data.previous_rest_url;
+                }
+            });
+        
+            console.log('async operation has ended');
+            infiniteScroll.complete();
+        }, 500);
     }
 
     ionViewDidLoad() {
