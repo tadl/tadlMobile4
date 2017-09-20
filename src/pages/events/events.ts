159b4e69ca92789d4b086d8884f873ab4ec616d9
William Rockwood <wrockwood@tadl.org>
2017-09-20 02:15:11 +0000

    rework events a bit
    - handle running out of pages correctly
    - reverse order of some of the things in the http requests
      it was possible to scroll glitch it into loading 2 of the
      same page sometimes.
    
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- src/pages/events/events.ts
+++ src/pages/events/events.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, Nav, LoadingController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, Content, NavController, NavParams, Nav, LoadingController } from 'ionic-angular';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
@@ -10,6 +10,7 @@ import 'rxjs/add/operator/map';
 })
 
 export class EventsPage {
+    @ViewChild(Content) content: Content;
 
     constructor(
         public navCtrl: NavController,
@@ -25,21 +26,21 @@ export class EventsPage {
     url: string = "https://www.tadl.org/wp-json/tribe/events/v1/events?start_date=now";
     next_url: string;
     prev_url: string;
+    lastPageReached: boolean = false;
 
     get_events() {
         let loading = this.loadingCtrl.create({content:'Loading events...'});
         loading.present();
         this.http.get(this.url).map(res => res.json()).subscribe(data=>{
-            loading.dismiss();
+            if (data.next_rest_url) {
+                this.next_url = data.next_rest_url;
+            } else {
+                this.lastPageReached = true;
+            }
             if (data.events) {
                 this.events = data.events;
             }
-            if (data.next_rest_url) {
-                this.next_url = data.next_rest_url;
-            }
-            if (data.previous_rest_url) {
-                this.prev_url = data.previous_rest_url;
-            }
+            loading.dismiss();
         });
     }
 
@@ -48,20 +49,28 @@ export class EventsPage {
         setTimeout(() => {
             this.url = this.next_url;
             this.http.get(this.url).map(res => res.json()).subscribe(data=>{
-                if (data.events) {
-                    this.events.push.apply(this.events, data.events);
-                }
                 if (data.next_rest_url) {
                     this.next_url = data.next_rest_url;
+                } else {
+                    this.next_url = ""
+                    this.lastPageReached = true;
                 }
-                if (data.previous_rest_url) {
-                    this.prev_url = data.previous_rest_url;
+                if (data.events) {
+                    this.events.push.apply(this.events, data.events);
                 }
             });
         
             console.log('async operation has ended');
             infiniteScroll.complete();
         }, 500);
+    }
+
+    isLastPageReached():boolean {
+        return this.lastPageReached;
+    }
+
+    scrollToTop() {
+        this.content.scrollToTop();
     }
 
     ionViewDidLoad() {
