adbdb1d8d95c9b57d81e77a0656cd2d2fc6f9cdf
William Rockwood <wrockwood@tadl.org>
2017-10-17 11:22:27 -0400

    added status messages to init script
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- apps/init.sh
+++ apps/init.sh
@@ -9,21 +9,34 @@ if [[ $target == "kcl" || $target == "sbbdl" ]]; then
 
     echo "Preparing build for ${target}"
 
+    echo "Changing to workdir: ${base}"
     cd $base
 
+    echo "Removing previous build (if exists)"
+    rm -rf ${target}Mobile
+
+    echo "Checking out tadlMobile4 as ${target}Mobile"
     git clone https://github.com/tadl/tadlMobile4.git ${target}Mobile
 
+    echo "Changing to workdir: ${target}Mobile"
     cd ${target}Mobile
 
+    echo "Copying ${target} configs and settings to app"
     cp -Rv apps/${target}/* .
 
+    echo "Running npm install"
     npm install
 
+    echo "Adding ionic remote: git@git.ionicjs.com:tadltech/${target}.git"
     git remote add ionic git@git.ionicjs.com:tadltech/${target}.git
 
+    echo "Adding ios platform"
     ionic cordova platform add ios
+
+    echo "Adding android platform"
     ionic cordova platform add android
-    ionic cordova resources
+
+    echo "Done! You may now cd ${base}/${target}Mobile and do 'ionic serve' to test!"
 
 else
     echo "You must specify one of: kcl, sbbdl"
