82fe14689fbc51ad2439ea96cd00700469c0c0fb
William Rockwood <wrockwood@tadl.org>
2017-10-19 19:49:51 -0400

    improve init script slightly
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- apps/init.sh
+++ apps/init.sh
@@ -3,26 +3,26 @@
 target=$1
 base=$HOME/deploy
 
-mkdir -p $base || exit
+mkdir -p "$base" || exit
 
 if [[ $target == "kcl" || $target == "sbbdl" ]]; then
 
     echo "Preparing build for ${target}"
 
     echo "Changing to workdir: ${base}"
-    cd $base || exit
+    cd "$base" || exit
 
     echo "Removing previous build (if exists)"
-    rm -rf ${base}/${target}Mobile
+    rm -rf "${base}/${target}Mobile"
 
     echo "Checking out tadlMobile4 as ${target}Mobile"
-    git clone https://github.com/tadl/tadlMobile4.git ${target}Mobile
+    git clone https://github.com/tadl/tadlMobile4.git "${target}Mobile"
 
     echo "Changing to workdir: ${target}Mobile"
-    cd ${target}Mobile || exit
+    cd "${target}Mobile" || exit
 
     echo "Copying ${target} configs and settings to app"
-    cp -Rv apps/${target}/* .
+    cp -Rv "apps/${target}/*" .
 
     echo "Running npm install"
     npm install
@@ -41,14 +41,16 @@ if [[ $target == "kcl" || $target == "sbbdl" ]]; then
     rm -rf .git
     git init
     git add .
-    git commit -m "deploy for ${target} on `date`"
+    git commit -m "deploy for ${target} on $(date)"
 
     echo "Adding ionic remote: git@git.ionicjs.com:tadltech/${target}.git"
-    git remote add ionic git@git.ionicjs.com:tadltech/${target}.git
+    git remote add ionic "git@git.ionicjs.com:tadltech/${target}.git"
 
     echo "Done! You may now cd ${base}/${target}Mobile and do 'ionic serve' to test!"
 
 else
+
     echo "You must specify one of: kcl, sbbdl"
+
 fi
 
