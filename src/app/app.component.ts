01a8ddaa10222c8b6631990a4e61ace0b4ac17bc
William Rockwood <wrockwood@tadl.org>
2017-10-20 14:19:35 -0400

    update cordova-plugin-statusbar and set viewport-fit=cover in index.html
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -55,6 +55,7 @@ export class MyApp {
             // Okay, so the platform is ready and our plugins are available.
             // Here you can do any higher level native things you might need.
             this.statusBar.styleDefault();
+            this.statusBar.overlaysWebView(false);
             this.user.check_legacy_storage();
             setTimeout(() => {
                 this.splashScreen.hide();
